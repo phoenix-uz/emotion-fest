65675edab65f4c61a179691c9ae228536b774815
Anna <a.khripchenko@student.inha.uz>
2024-07-24 16:07:32 +0500

    add main icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+       <link rel="icon" href="/logoico.svg" />
+      </head>
       <body className={montserrat.className}>
         <Header/>{children}<Footer/>
         </body>
